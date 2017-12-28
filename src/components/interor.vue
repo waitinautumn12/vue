08189fa3bb437f87ef55b3666c0f3d5565c2064a
songhaipeng <shp@autoforce.net>
2017-12-28 19:02:47 +0800

    内饰热点

--- src/components/interor.vue
+++ src/components/interor.vue
@@ -1,9 +1,6 @@
 <template>
   <div style="width:100vw;height:100vh;">
-    <!-- <a href="javascript:history.back(-1)">
-      <div style="width:40px;height:40px;background:red;position:fixed;left:30px;"></div>
-    </a> -->
-    <iframe id="vr" src="src/vr/ns1/" frameborder="0" width="100%" height="100%"> </iframe>
+    <iframe id="vr" src="src/vr/ns3/" frameborder="0" width="100%" height="100%"> </iframe>
   </div>
 </template>
 
@@ -15,15 +12,16 @@ export default {
       active: true
     }
   },
-  methods: {
-
+  created () {
+    window.addEventListener('message', function (e) {
+      var id = e.data
+      console.log(id)
+    }, false)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
 
 </style>
